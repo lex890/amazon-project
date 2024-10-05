d594854ad733a7e2d919cb611b59b8df2c0be3c4
Lexter Hiteroza <ashketchopstick@gmail.com>
2024-10-05 21:42:00 +0800

    add testing folder

---
+++ scripts/tests/testConvert.js
@@ -0,0 +1,26 @@
+import {convertMoney} from '../utils/money.js';
+
+console.log('money conversion testing');
+
+
+console.log('Normal Number');
+if (convertMoney(2095) === '20.95') {
+  console.log('success');
+} else {
+  console.log('failed');
+}
+
+console.log('Zero Number');
+if (convertMoney(0) === '0.00') {
+  console.log('success');
+} else {
+  console.log('failed');
+}
+
+console.log('Partial Cents');
+if (convertMoney(2000.5) === '20.01') {
+  console.log('success');
+} else {
+  console.log('failed');
+}
+
