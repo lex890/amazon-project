c69ba97f19abb57ec002babfe0c296be321a1bbb
Lexter Hiteroza <ashketchopstick@gmail.com>
2025-01-02 22:37:18 +0800

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {displayOrder} from './checkout/orderSummary.js';
 import {displayPayment} from './checkout/paymentSummary.js';
 import {displayCheckout} from './checkout/checkoutHeader.js';
+import '../data/backend-practice.js';
 
 displayOrder();
 displayPayment();
